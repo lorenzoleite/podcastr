511bde609abf50bdc86f34facd825e40d3d35669
Lorenzo Leite <lorenzokleite@gmail.com>
2021-04-23 02:54:28 -0300

    Static Dinamic Pages Generation and Controls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,32 @@
 import '../styles/global.scss';
 
+import { useState } from 'react';
+
 import { Header } from '../components/Header'; //auto import por não ter default no index do header
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, SetCurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    SetCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
